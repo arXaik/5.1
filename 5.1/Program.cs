00f787de9bbec0cbbe5141f09ae920bd1dd62db2
arXaik <araik200414@gmail.com>
2022-12-04 18:17:57 +0200

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Петросян 5.1 Средний уровень 16 вар
+Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+int L = str.Length;
+int i;
+for (i = 0;i < L;i++)
+    {
+        str = str.Replace("  ", " ");
+    }
+Console.WriteLine(str);
